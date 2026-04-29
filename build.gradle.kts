19ddf2afcb9fc76a2c4f29880ccd9585e75a06e9
Zax71 <67716263+zax71@users.noreply.github.com>
2026-04-29 16:27:59 +0100

    Update shadowjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     // ShadowJar (https://github.com/johnrengelman/shadow/releases)
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.4.1"
 }
 
 group = "me.zax71"
